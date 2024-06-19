9ee21821958bf751cd36df55e79c06a000ae3d71
Om D Jasoliya <107740911+omjasoliya@users.noreply.github.com>
2024-06-19 15:54:14 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
+from .main import main as main_blueprint
 
 def create_app():
     app = Flask(__name__)
-    from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     return app
